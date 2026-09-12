160846ed8e8ffc43ee0e7b537dedd51b0f41b819
arvectum2 <g46mimu7ns@crassusorder.shop>
2026-09-12 21:41:18 +0300

    APL-WIN hotfix: point GUI helper at LocalAppData install

--- run_gui.bat
+++ run_gui.bat
@@ -1,8 +1,11 @@
 @echo off
 cd /d "%~dp0"
-set "INSTALLED_EXE=%USERPROFILE%\Documents\ArvectumProxyLauncher\Arvectum Proxy Launcher.exe"
+set "INSTALLED_EXE=%LOCALAPPDATA%\Programs\ArvectumProxyLauncher\Arvectum Proxy Launcher.exe"
+set "LEGACY_EXE=%USERPROFILE%\Documents\ArvectumProxyLauncher\Arvectum Proxy Launcher.exe"
 if exist "%INSTALLED_EXE%" (
     start "" "%INSTALLED_EXE%"
+) else if exist "%LEGACY_EXE%" (
+    start "" "%LEGACY_EXE%"
 ) else if exist "%~dp0Arvectum Proxy Launcher.exe" (
     start "" "%~dp0Arvectum Proxy Launcher.exe"
 ) else if exist "%~dp0ProxyLauncher.exe" (
