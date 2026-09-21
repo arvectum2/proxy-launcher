0a556b5b00dbe3e0a541c02fe64eeac168d789e5
arvectum2 <g46mimu7ns@crassusorder.shop>
2026-09-21 18:38:17 +0300

    chore(android): bump friend-test candidate to 0.1.31

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -145,8 +145,8 @@ android {
         applicationId = "ru.arvectum.proxylauncher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "0.1.30"
+        versionCode = 32
+        versionName = "0.1.31"
     }
 
     signingConfigs {
