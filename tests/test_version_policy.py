49ea4c420559ba20f02a5637ffad457d7841e95d
arutyunoveth <arutyunov.eth@gmail.com>
2026-09-21 20:24:34 +0300

    chore(release): prepare v0.2.12

--- tests/test_version_policy.py
+++ tests/test_version_policy.py
@@ -62,7 +62,7 @@ class VersionPolicyTests(unittest.TestCase):
 
     def test_active_platform_builders_use_canonical_version_source(self):
         version = self.get_version()
-        self.assertEqual(version, "0.2.11")
+        self.assertEqual(version, "0.2.12")
 
         live_paths = {
             "macOS app": "tools/build_macos_app.sh",
