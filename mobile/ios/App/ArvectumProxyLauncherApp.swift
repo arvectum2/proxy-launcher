2d760d0086beb01667f74ed32a6ea4bc96a400bb
arutyunoveth <arutyunov.eth@gmail.com>
2026-09-25 11:29:53 +0300

    iOS: prepare App Store privacy and review metadata

--- mobile/ios/App/ArvectumProxyLauncherApp.swift
+++ mobile/ios/App/ArvectumProxyLauncherApp.swift
@@ -3,11 +3,20 @@ import SwiftUI
 @main
 struct ArvectumProxyLauncherApp: App {
     @StateObject private var model = AppViewModel()
+    @AppStorage("privacyDisclosureAccepted.v1") private var privacyDisclosureAccepted = false
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environmentObject(model)
+            Group {
+                if privacyDisclosureAccepted {
+                    ContentView()
+                } else {
+                    PrivacyDisclosureView {
+                        privacyDisclosureAccepted = true
+                    }
+                }
+            }
+            .environmentObject(model)
         }
     }
 }
